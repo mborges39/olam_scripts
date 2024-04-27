861815c592d92ca5a5d58ee9cc06073fb8df50bb
mborges39 <marcelo.borges@live.com>
2024-04-27 09:48:29 -0300

    Criação do copy_v2 e correcao do log - criado log para cada arquivo copiado

--- copy_file_ v2.py
+++ copy_file_ v2.py
@@ -17,9 +17,13 @@ def copiar_arquivos(origem, destino):
             shutil.copy2(caminho_origem, caminho_destino) # shutil.copy2 - Preserva os metadata
             print(f"Arquivo {arquivo} copiado com sucesso!")
 
+            # Grava um log
+            with open("log.txt", "a") as log:
+                log.write(f"Cópia do arquivo {arquivo} realizada {origem} para {destino}.\n")
+
         # Grava um log
-        with open("log.txt", "a") as log:
-            log.write(f"Cópia de arquivos de {origem} para {destino} realizada.\n")
+       # with open("log.txt", "a") as log:
+        #    log.write(f"Cópia de arquivos de {origem} para {destino} realizada.\n")
 
     except Exception as e:
         print(f"Erro ao copiar arquivos: {e}")
@@ -28,6 +32,6 @@ def copiar_arquivos(origem, destino):
 # pasta_origem = "caminho/para/sua/pasta/origem"
 # pasta_destino = "caminho/para/sua/pasta/destino"
 
-pasta_origem = "C://Users//marce//iCloudPhotos//Photos"
-pasta_destino = "C://Backup_Fotos_iCloud"
+pasta_origem = "C://Pasta_de_Teste//Origem"
+pasta_destino = "C://Pasta_de_Teste//Destino"
 copiar_arquivos(pasta_origem, pasta_destino)
